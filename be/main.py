7499103a94a3209d5a4edb5654c91710e333c8bd
GangBean <xbox2537@gmail.com>
2024-12-14 14:45:05 +0900

    feat: LLM 기반 카테고리 자동 분류 추가

--- be/main.py
+++ be/main.py
@@ -7,6 +7,17 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from typing import List
 import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain_teddynote.messages import stream_response
+from langchain_teddynote import logging
+from langchain.output_parsers import PydanticOutputParser
+
+load_dotenv()
+
+# 프로젝트 이름을 입력합니다.
+logging.langsmith("lunch-lens", "0.1.0")
 
 app = FastAPI()
 
@@ -15,6 +26,9 @@ SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://admin:admin123
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+# llm 생성
+llm = ChatOpenAI(temperature=0, model_name="gpt-4-turbo")
 
 # 데이터베이스 모델
 class FoodMetadata(Base):
@@ -57,12 +71,41 @@ def get_db():
     finally:
         db.close()
 
+class FoodCategory(BaseModel):
+    food_name: str
+    category: str
+
+def get_food_category(food_name: str) -> str:
+    categories = ["한식", "중식", "양식", "일식", "기타"]
+    try:
+        parser = PydanticOutputParser(pydantic_object=FoodCategory)
+        prompt_template = PromptTemplate(
+            template="What is the category of {food_name}? The category should be one of {categories}. format={format_instructions}",
+            input_variables=["food_name"],
+            partial_variables={"categories": categories, "format_instructions": parser.get_format_instructions()},
+        )
+        chain = prompt_template | llm | parser
+        answer: FoodCategory = chain.invoke({"food_name": food_name})
+        return answer.category
+    except Exception as e:
+        raise e
+        # return "Unknown"
+
 @app.post("/log-food", response_model=FoodLogResponse)
 def log_food(food: FoodLogCreate, db: Session = Depends(get_db)):
     # 음식이 메타데이터에 있는지 확인
     food_exists = db.query(FoodMetadata).filter(FoodMetadata.food_name == food.food_name).first()
+    
+    # 메타데이터에 없는 경우 자동으로 카테고리 생성 후 추가
     if not food_exists:
-        raise HTTPException(status_code=404, detail="Food not found in metadata")
+        category = get_food_category(food.food_name)
+        new_food_metadata = FoodMetadata(
+            food_name=food.food_name,
+            category=category
+        )
+        db.add(new_food_metadata)
+        db.commit()
+        db.refresh(new_food_metadata)
     
     db_food_log = FoodLog(
         user_id=food.user_id,
